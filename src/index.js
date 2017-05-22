3402107099308e70174cf84139575908ec0f65c2
jicjjang <jicjjang12@gmail.com>
2017-05-23 00:52:26 +0900

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = {
+var axios = require('axios');
+var uniqueRandomArray = require('unique-random-array');
 
+var datas = [];
+
+axios.get('https://api.github.com/users').then(function(res) {
+  var names = [];
+  res.data.map(function (val, key) {
+    datas.push(val.login);
+  })
+})
+
+module.exports = {
+  getDatas: function () {
+    return datas;
+  },
+  getRandomDatas: function () {
+    return uniqueRandomArray(datas)();
+  }
 }
